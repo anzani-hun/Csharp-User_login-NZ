c63ba0143241e4ce0cfa80d80fd31b740b20827a
anzani-hun <anzani@hun>
2024-04-17 19:20:53 +0200

    ora vége

---
+++ Csharp-User_login-NZ/Adatbazis.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MySqlConnector;
+
+namespace Csharp_User_login_NZ
+{
+
+    class Adatbazis
+    {
+
+        MySqlConnection dbConnection;      // adatbázis kapcsolat létrehozása
+        MySqlCommand sqlCommand;            // a lekérdezése parancs kapcsolata         
+
+        public Adatbazis()
+        {
+
+            string connectionString = "server=localhost;port=3306;uid=userloginclient;password=almaeper;database=userloginapp";
+
+            dbConnection = new MySqlConnection(connectionString);
+            dbConnection.Open();
+
+            sqlCommand = dbConnection.CreateCommand();
+            sqlCommand.CommandText = "SELECT * FROM userdata";
+
+
+
+        }
+    }
+}
---
+++ Csharp-User_login-NZ/Userdata.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_User_login_NZ
+{
+    class Userdata
+    {
+        int userid;
+        string username;
+        string email;
+        string password;
+
+        public Userdata(int userid, string username, string email, string password)
+        {
+            this.userid = userid;
+            this.username = username;
+            this.email = email;
+            this.password = password;
+        }
+
+        public int Userid { get => userid; set => userid = value; }
+        public string Username { get => username; set => username = value; }
+        public string Email { get => email; set => email = value; }
+        public string Password { get => password; set => password = value; }
+    }
+}

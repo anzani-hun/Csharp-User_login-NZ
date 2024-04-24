ea56d0cdeccbea1d151e76f21dad4fd30b31d5ec
anzani-hun <anzani@hun>
2024-04-24 19:16:02 +0200

    felhasználónév és jelszó ellenorzése okés

--- Csharp-User_login-NZ/Adatbazis.cs
+++ Csharp-User_login-NZ/Adatbazis.cs
@@ -12,22 +12,41 @@ namespace Csharp_User_login_NZ
     class Adatbazis
     {
 
-        MySqlConnection dbConnection;      // adatbázis kapcsolat létrehozása
-        MySqlCommand sqlCommand;            // a lekérdezése parancs kapcsolata         
-
-        public Adatbazis()
+        //adatbázis kapcsolat és adatbázis SELECT futtatása
+        MySqlConnectionStringBuilder connStr = new MySqlConnectionStringBuilder()
         {
+            Server = "localhost",
+            Port = 3306,
+            UserID = "userloginclient",
+            Password = "almaeper",
+            Database = "userloginapp"
+        };
+        MySqlConnection Connect { get => new MySqlConnection(connStr.ConnectionString); }
+        MySqlCommand cmd = null;
 
-            string connectionString = "server=localhost;port=3306;uid=userloginclient;password=almaeper;database=userloginapp";
-
-            dbConnection = new MySqlConnection(connectionString);
-            dbConnection.Open();
-
-            sqlCommand = dbConnection.CreateCommand();
-            sqlCommand.CommandText = "SELECT * FROM userdata";
 
 
+        //lista létrehozása a  betöltésére
+        public List<Userdata> userLista()
+        {
+            MySqlConnection conn = Connect;
+            List<Userdata> users = new List<Userdata>();
+            cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT * FROM userdata;";
 
+            conn.Open();
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Userdata user = new Userdata(reader.GetInt32("userid"), reader.GetString("username"), reader.GetString("email"), reader.GetString("password"));
+                users.Add(user);
+            }
+            conn.Close();
+
+            return users;
         }
+
+
     }
 }
--- Csharp-User_login-NZ/Program.cs
+++ Csharp-User_login-NZ/Program.cs
@@ -10,8 +10,77 @@ namespace Csharp_User_login_NZ
 {
     class Program
     {
+
+        // az adatbázis példányosítása:
+        static Adatbazis db = new Adatbazis();
+        static List<Userdata> userdatas = db.userLista();
+
         static void Main(string[] args)
         {
+
+            /*TESZT
+            foreach (Userdata adat in userdatas)
+            {
+                Console.WriteLine(adat.Username);
+            }
+            */
+
+                Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("***Sziper-Szuper bejelentkező program 4000***");
+            Console.WriteLine("Kérem adja meg a bejelentkezési adatait");
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Write("Felhasználónév: ");
+            string felhasznalonev = Console.ReadLine();
+
+            Console.Write("Jelszó: ");
+            string jelszo = null;
+
+
+            while (true)
+            {
+                var key = System.Console.ReadKey(true);
+                if (key.Key == ConsoleKey.Enter)
+                    break;
+                jelszo = jelszo + key.KeyChar;
+            }
+
+
+            feldolgoz1(felhasznalonev, jelszo);
+
+            Console.ReadKey();
+        }
+
+        public static void feldolgoz1(string felhasznalonev, string jelszo)
+        {
+
+            //bool változó alapból false-ra állítva.
+            bool logged = false; 
+
+            foreach (Userdata adat in userdatas)
+            {
+                if (felhasznalonev == adat.Username)
+                {
+                    if (jelszo == adat.Password)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("\nSikeres bejelentkezés!");
+                        logged = true;
+                        break;
+                    }
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Rossz jelszó!");
+                }
+            }
+
+            if (!logged)
+            {
+                Console.WriteLine("Sikertelen bejelentkezés!");
+            }
+
+            Console.ReadKey();
         }
     }
+
+    
 }
